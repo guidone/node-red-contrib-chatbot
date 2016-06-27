5b5268157eb32908c44087d1a3693617904a4026
Guidone <guido@gild.com>
2016-06-27 16:40:45 +0200

    fix

--- chatbot-parse.js
+++ chatbot-parse.js
@@ -39,7 +39,7 @@ module.exports = function(RED) {
       // if parsing ok, then pass through and set variable in context flow
       if (parsedValue != null) {
         context.flow.set(parseVariable, parsedValue);
-        node.send([parsedValue, null]);
+        node.send([{payload: parsedValue}, null]);
       } else {
         node.send([null, msg]);
       }
