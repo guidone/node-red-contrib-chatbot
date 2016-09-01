a1fb607b0ba05b73f2cf2e932a24a283cbf36196
Luca <khadgar1@gmail.com>
2016-09-01 12:04:53 +0200

    Update chatbot-facebook-receive.js

--- chatbot-facebook-receive.js
+++ chatbot-facebook-receive.js
@@ -141,7 +141,7 @@ module.exports = function(RED) {
             key_pem: this.key_pem,
             cert_pem: this.cert_pem
           });
-          console.warn('Running webhook on https://localhost:61626');
+          console.warn('Running webhook on https://localhost:3099');
           console.warn('Verify token is: ' + this.verify_token);
           console.warn('Key PEM: ' + this.key_pem);
           console.warn('Cert PEM: ' + this.cert_pem);
@@ -152,7 +152,7 @@ module.exports = function(RED) {
           };
 
           this.server = https.createServer(options,
-          this.bot.middleware()).listen(61626);
+          this.bot.middleware()).listen(3099);
 
           this.bot.on('message', this.handleMessage);
 
