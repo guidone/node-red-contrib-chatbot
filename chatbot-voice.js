6eec1671c3fc6d89554f513c2e93a1c5c5361902
Guidone <guido@gild.com>
2016-09-28 20:40:27 +0200

    Squashed commit of the following:
    commit bb5ca4aa785c00eb82affd704c128105f229e26a
    Author: Guidone <guido@gild.com>
    Date:   Thu Sep 22 18:48:30 2016 +0200
    
        add icon
    
    commit 05c993a6c862fef3a9f85d2b545d8e6c8e1626a6
    Author: Guidone <guido@gild.com>
    Date:   Thu Sep 22 18:45:36 2016 +0200
    
        voice node

---
+++ chatbot-voice.js
@@ -0,0 +1,65 @@
+var _ = require('underscore');
+var MessageTemplate = require('./lib/message-template.js');
+var request = require('request').defaults({ encoding: null });
+
+module.exports = function(RED) {
+
+  function ChatBotVoice(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    this.message = config.message;
+    this.language = config.language;
+    this.transports = ['telegram', 'slack', 'facebook', 'smooch'];
+
+    this.on('input', function(msg) {
+      var message = node.message;
+      var language = node.language;
+      var originalMessage = msg.originalMessage;
+      var chatId = msg.payload.chatId || (originalMessage && originalMessage.chat.id);
+      var messageId = msg.payload.messageId || (originalMessage && originalMessage.message_id);
+      var template = MessageTemplate(msg, node);
+
+      // check transport compatibility
+      if (!_.contains(node.transports, msg.originalMessage.transport)) {
+        node.error('This node is not available for transport: ' + msg.originalMessage.transport);
+        return;
+      }
+
+      if (!_.isEmpty(node.message)) {
+        message = node.message;
+      } else if (_.isString(msg.payload) && !_.isEmpty(msg.payload)) {
+        message = msg.payload;
+      } else {
+        node.error('Empty message');
+      }
+
+      var parsedMessage = template(message);
+      var voiceUrl = 'http://www.voicerss.org/controls/speech.ashx?'
+        + 'hl=' + language
+        + '&src=' + encodeURI(parsedMessage) + '&c=mp3'
+        + '&rnd=' + Math.random();
+
+      request.get({
+        url: voiceUrl,
+        headers: {}
+      }, function(err, response, buffer) {
+        if (err) {
+          node.error('Error contacting VoiceRSS');
+        } else {
+          msg.payload = {
+            type: 'audio',
+            content: buffer,
+            filename: 'audio.mp3',
+            chatId: chatId,
+            messageId: messageId,
+            inbound: false
+          };
+          node.send(msg);
+        }
+      });
+
+    });
+  }
+
+  RED.nodes.registerType('chatbot-voice', ChatBotVoice);
+};
