dd4468aef589eccbd00adc16a53f9435ecb326d8
Guidone <guido@gild.com>
2017-07-28 10:08:30 +0200

    list template

---
+++ chatbot-list-template.js
@@ -0,0 +1,67 @@
+var _ = require('underscore');
+var MessageTemplate = require('./lib/message-template.js');
+var utils = require('./lib/helpers/utils');
+var validators = require('./lib/helpers/validators');
+
+module.exports = function(RED) {
+
+  function ChatBotListTemplate(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    this.name = config.name;
+    this.buttons = config.buttons;
+    this.title = config.title;
+    this.subtitle = config.subtitle;
+    this.imageUrl = config.imageUrl;
+    this.aspectRatio = config.aspectRatio;
+    this.sharable = config.sharable;
+    this.transports = ['facebook'];
+
+    this.on('input', function(msg) {
+
+      // check transport compatibility
+      if (!utils.matchTransport(node, msg)) {
+        return;
+      }
+
+      var chatId = utils.getChatId(msg);
+      var messageId = utils.getMessageId(msg);
+      var template = MessageTemplate(msg, node);
+
+      // get values from config
+      var buttons = utils.extractValue('buttons', 'buttons', node, msg);
+      var title = utils.extractValue('string', 'title', node, msg);
+      var subtitle = utils.extractValue('string', 'subtitle', node, msg);
+      var imageUrl = utils.extractValue('string', 'imageUrl', node, msg);
+      var topElementStyle = utils.extractValue('string', 'topElementStyle', node, msg);
+      var sharable = utils.extractValue('boolean', 'sharable', node, msg);
+
+      var elements = [];
+      // if inbound is another message from a generic template, then push them toghether to create a carousel
+      if (msg.payload != null && validators.genericTemplateElements(msg.payload.elements)) {
+        elements = _.union(elements, msg.payload.elements);
+      }
+      // add the current one
+      elements.push({
+        title: template(title),
+        subtitle: template(subtitle),
+        imageUrl: template(imageUrl),
+        buttons: buttons
+      });
+
+      msg.payload = {
+        type: 'list-template',
+        topElementStyle: !_.isEmpty(topElementStyle) ? topElementStyle : 'large',
+        sharable: _.isBoolean(sharable) ? sharable : true,
+        elements: elements,
+        chatId: chatId,
+        messageId: messageId
+      };
+
+      node.send(msg);
+    });
+
+  }
+
+  RED.nodes.registerType('chatbot-list-template', ChatBotListTemplate);
+};
