75f5ba99dc05e015d5f1434990b7513d59591dac
Guidone <guido@gild.com>
2017-08-04 17:27:26 +0200

    delete

--- chatbot-messenger-menu.js
+++ chatbot-messenger-menu.js
@@ -1,4 +1,5 @@
 var utils = require('./lib/helpers/utils');
+var _ = require('underscore');
 
 module.exports = function(RED) {
 
@@ -6,6 +7,8 @@ module.exports = function(RED) {
     RED.nodes.createNode(this, config);
     var node = this;
     this.items = config.items;
+    this.command = config.command;
+    this.composerInputDisabled = config.composerInputDisabled;
     this.transports = ['facebook'];
 
     this.on('input', function(msg) {
@@ -13,13 +16,17 @@ module.exports = function(RED) {
       var chatId = utils.getChatId(msg);
       var messageId = utils.getMessageId(msg);
       var items = utils.extractValue('buttons', 'items', node, msg);
+      var command = utils.extractValue('string', 'command', node, msg);
+      var composerInputDisabled = utils.extractValue('boolean', 'composerInputDisabled', node, msg);
 
       // payload
       msg.payload = {
         type: 'persistent-menu',
         chatId: chatId,
         messageId: messageId,
-        items: items
+        items: items,
+        command: !_.isEmpty(command) ? command : 'set',
+        composerInputDisabled: _.isBoolean(composerInputDisabled) ? composerInputDisabled : false
       };
 
       node.send(msg);
