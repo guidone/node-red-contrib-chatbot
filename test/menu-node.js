f5c1a8566f83b5ba165d99cbbdd15d5554af3d26
Guidone <guido@gild.com>
2017-08-05 09:42:15 +0200

    test

--- test/menu-node.js
+++ test/menu-node.js
@@ -6,8 +6,9 @@ var MenuBlock = require('../chatbot-messenger-menu');
 describe('Chat menu node', function() {
 
   it('should create a persistent menu in facebook', function() {
-    var msg = RED.createMessage(null, 'facebook');
+    var msg = RED.createMessage({}, 'facebook');
     RED.node.config({
+      composerInputDisabled: false,
       items: [
         {
           type: 'url',
@@ -37,6 +38,8 @@ describe('Chat menu node', function() {
     assert.equal(RED.node.message(0).payload.items[1].type, 'postback');
     assert.equal(RED.node.message(0).payload.items[1].title, 'Help');
     assert.equal(RED.node.message(0).payload.items[1].payload, '/help');
+    assert.equal(RED.node.message(0).payload.command, 'set');
+    assert.equal(RED.node.message(0).payload.composerInputDisabled, false);
 
   });
 
@@ -70,7 +73,46 @@ describe('Chat menu node', function() {
     assert.equal(RED.node.message(0).payload.items[1].type, 'postback');
     assert.equal(RED.node.message(0).payload.items[1].title, 'Help');
     assert.equal(RED.node.message(0).payload.items[1].payload, '/help');
+    assert.equal(RED.node.message(0).payload.command, 'set');
+    assert.equal(RED.node.message(0).payload.composerInputDisabled, false);
+  });
 
+  it('should create a persistent menu in facebook passed by the upstream node in payload (all)', function() {
+    var msg = RED.createMessage({
+      composerInputDisabled: true,
+      command: 'set',
+      items: [
+        {
+          type: 'url',
+          title: 'JavaScript Jedi',
+          url: 'http://javascript-jedi.com',
+          webview_height_ratio: 'full' // tall|full|compact
+        },
+        {
+          type: 'postback',
+          title: 'Help',
+          payload: '/help'
+        }
+      ]
+    }, 'facebook');
+    RED.node.config({});
+    MenuBlock(RED);
+    RED.node.get().emit('input', msg);
+
+    assert.equal(RED.node.message(0).originalMessage.chat.id, '42');
+    assert.equal(RED.node.message(0).originalMessage.transport, 'facebook');
+    assert.equal(RED.node.message(0).payload.type, 'persistent-menu');
+    assert.isArray(RED.node.message(0).payload.items);
+    assert.isObject(RED.node.message(0).payload.items[0]);
+    assert.equal(RED.node.message(0).payload.items[0].type, 'url');
+    assert.equal(RED.node.message(0).payload.items[0].title, 'JavaScript Jedi');
+    assert.equal(RED.node.message(0).payload.items[0].webview_height_ratio, 'full');
+    assert.isObject(RED.node.message(0).payload.items[1]);
+    assert.equal(RED.node.message(0).payload.items[1].type, 'postback');
+    assert.equal(RED.node.message(0).payload.items[1].title, 'Help');
+    assert.equal(RED.node.message(0).payload.items[1].payload, '/help');
+    assert.equal(RED.node.message(0).payload.command, 'set');
+    assert.equal(RED.node.message(0).payload.composerInputDisabled, true);
   });
 
 });
