b7bd0dba1e373f98f0de70e36e1856d7a5495c7f
Guidone <guido.bellomo@gmail.com>
2017-11-21 08:05:26 +0100

    fix listen node

--- test/listen-node.js
+++ test/listen-node.js
@@ -16,11 +16,13 @@ describe('Chat listen node', function() {
     });
     ListenBlock(RED);
     RED.node.get().emit('input', msg);
-
-    assert.equal(RED.node.message(0).payload.content, '100%');
-    assert.isNull(RED.node.message(1));
-    assert.isNull(RED.node.message(2));
-    assert.equal(msg.chat().get('tipPercentage'), 100);
+    return RED.node.get().await()
+      .then(function () {
+        assert.equal(RED.node.message(0).payload.content, '100%');
+        assert.isNull(RED.node.message(1));
+        assert.isNull(RED.node.message(2));
+        assert.equal(msg.chat().get('tipPercentage'), 100);
+      });
   });
 
   it('should not try to parse a command-like message', function () {
@@ -33,9 +35,14 @@ describe('Chat listen node', function() {
     });
     ListenBlock(RED);
     RED.node.get().emit('input', msg);
-
-    assert.isNull(RED.node.message(0));
-    assert.isNull(RED.node.message(1));
+    return RED.node.get().await()
+      .then(
+        function() {},
+        function () {
+          assert.isNull(RED.node.message(0));
+          assert.isNull(RED.node.message(1));
+        }
+      );
   });
 
   it('should detect a simple phrase send curriculum', function () {
@@ -45,8 +52,11 @@ describe('Chat listen node', function() {
     });
     ListenBlock(RED);
     RED.node.get().emit('input', msg);
-    assert.equal(RED.node.message(0).payload.content, 'can you send your curriculum vitae');
-    assert.equal(RED.node.message(0).originalMessage.chat.id, 42);
+    return RED.node.get().await()
+      .then(function () {
+        assert.equal(RED.node.message(0).payload.content, 'can you send your curriculum vitae');
+        assert.equal(RED.node.message(0).originalMessage.chat.id, 42);
+      });
   });
 
   it('should detect a composed phrase send curriculum', function () {
@@ -56,8 +66,11 @@ describe('Chat listen node', function() {
     });
     ListenBlock(RED);
     RED.node.get().emit('input', msg);
-    assert.equal(RED.node.message().payload.content, 'can you send your curriculum vitae');
-    assert.equal(RED.node.message().originalMessage.chat.id, 42);
+    return RED.node.get().await()
+      .then(function () {
+        assert.equal(RED.node.message().payload.content, 'can you send your curriculum vitae');
+        assert.equal(RED.node.message().originalMessage.chat.id, 42);
+      });
   });
 
   it('should not detect a composed phrase send curriculum', function () {
@@ -67,7 +80,10 @@ describe('Chat listen node', function() {
     });
     ListenBlock(RED);
     RED.node.get().emit('input', msg);
-    assert.equal(RED.node.message(), null);
+    return RED.node.get().await()
+      .then(function () {
+        assert.equal(RED.node.message(), null);
+      });
   });
 
   it('should detect a composed phrase send curriculum and extract email', function () {
@@ -78,9 +94,12 @@ describe('Chat listen node', function() {
     msg.chat().set({authorized: true});
     ListenBlock(RED);
     RED.node.get().emit('input', msg);
-    assert.equal(RED.node.message().payload.content, 'can you send your curriculum vitae to guido.bellomo@gmail.com');
-    assert.equal(RED.node.message().originalMessage.chat.id, 42);
-    assert.equal(msg.chat().get('email'), 'guido.bellomo@gmail.com');
+    return RED.node.get().await()
+      .then(function () {
+        assert.equal(RED.node.message().payload.content, 'can you send your curriculum vitae to guido.bellomo@gmail.com');
+        assert.equal(RED.node.message().originalMessage.chat.id, 42);
+        assert.equal(msg.chat().get('email'), 'guido.bellomo@gmail.com');
+      });
   });
 
   it('should NOT detect a composed phrase send curriculum and extract email', function () {
@@ -91,7 +110,10 @@ describe('Chat listen node', function() {
     msg.chat().set({authorized: true});
     ListenBlock(RED);
     RED.node.get().emit('input', msg);
-    assert.equal(RED.node.message(), null);
+    return RED.node.get().await()
+      .then(function () {
+        assert.equal(RED.node.message(), null);
+      });
   });
 
 });
