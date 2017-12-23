032428c6bdcf96e85188fcb3add93a80fff6a036
Guidone <guido.bellomo@gmail.com>
2017-12-23 12:58:51 +0100

    test + docs

--- nodes/chatbot-dialog.js
+++ nodes/chatbot-dialog.js
@@ -1,7 +1,4 @@
-var _ = require('underscore');
-var MessageTemplate = require('../lib/message-template-async');
 var utils = require('../lib/helpers/utils');
-var validators = require('../lib/helpers/validators');
 
 module.exports = function(RED) {
 
@@ -27,24 +24,6 @@ module.exports = function(RED) {
       var submitLabel = utils.extractValue('string', 'submitLabel', node, msg, false);
       var elements = utils.extractValue('arrayOfObject', 'elements', node, msg, true);
 
-
-      /*
-
-      [
-          {
-            "type": "text",
-            "label": "Pickup Location",
-            "name": "loc_origin"
-          },
-          {
-            "type": "text",
-            "label": "Dropoff Location",
-            "name": "loc_destination"
-          }
-        ]
-       */
-
-
       // payload
       msg.payload = {
         type: 'dialog',
@@ -55,37 +34,6 @@ module.exports = function(RED) {
         messageId: messageId
       };
       node.send(msg);
-
-
-      /*var elements = [];
-      // if inbound is another message from a generic template, then push them toghether to create a carousel
-      if (msg.payload != null && validators.genericTemplateElements(msg.payload.elements)) {
-        elements = _.union(elements, msg.payload.elements);
-      }*/
-
-      /*template(title, subtitle, imageUrl)
-        .then(function(translated) {
-          // add the current one if not empty
-          if (!_.isEmpty(translated[0])) {
-            elements.push({
-              title: translated[0],
-              subtitle: translated[1],
-              imageUrl: translated[2],
-              buttons: buttons
-            });
-          }
-          // prep payload
-          msg.payload = {
-            type: 'generic-template',
-            aspectRatio: !_.isEmpty(aspectRatio) ? aspectRatio : 'horizontal',
-            sharable: _.isBoolean(sharable) ? sharable : true,
-            elements: elements,
-            chatId: chatId,
-            messageId: messageId
-          };
-          node.send(msg);
-        });
-        */
     });
   }
 
