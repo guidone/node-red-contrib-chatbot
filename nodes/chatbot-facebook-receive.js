37144e899f9bcfdb9b741a5e45dbcf6d7b1fc234
Guidone <guido.bellomo@gmail.com>
2018-01-30 10:28:28 +0100

    fix telgram and facebook

--- nodes/chatbot-facebook-receive.js
+++ nodes/chatbot-facebook-receive.js
@@ -115,21 +115,29 @@ module.exports = function(RED) {
     this.bot = config.bot;
 
     this.config = RED.nodes.getNode(this.bot);
+    var global = this.context().global;
+    var nodeGlobalKey = null;
 
     if (this.config) {
       this.status({fill: 'red', shape: 'ring', text: 'disconnected'});
       node.chat = this.config.chat;
       if (node.chat) {
         this.status({fill: 'green', shape: 'ring', text: 'connected'});
+        nodeGlobalKey = 'facebook_master_' + this.config.id.replace('.','_');
+        var isMaster = false;
+        if (global.get(nodeGlobalKey) == null) {
+          isMaster = true;
+          global.set(nodeGlobalKey, node.id);
+        }
         node.chat.on('message', function (message) {
           var context = message.chat();
           // if a conversation is going on, go straight to the conversation node, otherwise if authorized
           // then first pin, if not second pin
           when(context.get('currentConversationNode'))
             .then(function(currentConversationNode) {
-              if (currentConversationNode != null) {
+              if (isMaster && currentConversationNode != null) {
                 // void the current conversation
-                when(context.set('currentConversationNode', null))
+                when(context.remove('currentConversationNode'))
                   .then(function() {
                     // emit message directly the node where the conversation stopped
                     RED.events.emit('node:' + currentConversationNode, message);
