cd42685c845c468fcd44e9c3becf0cad2010013d
Guidone <guido.bellomo@gmail.com>
2018-02-14 16:47:51 +0100

    Fix keyboard (#175)
    * refactor keyboard
    
    * fix tests

--- __tests__/ask-node.js
+++ __tests__/ask-node.js
@@ -40,6 +40,44 @@ describe('Chat keyword node', function() {
         assert.equal(RED.node.message().payload.buttons[2].value, 'value 3');
         assert.equal(RED.node.message().payload.buttons[2].label, 'Value 3');
       });
+  });
+
+  it('should send custom keyboard in Telegram passed thru payload', function () {
+    var msg = RED.createMessage({
+      message: 'message for the buttons',
+      buttons: [
+        {
+          value: 'value 1',
+          label: 'Value 1'
+        },
+        {
+          value: 'value 2',
+          label: 'Value 2'
+        },
+        {
+          value: 'value 3',
+          label: 'Value 3'
+        }
+      ]
+    }, 'telegram');
+
+    RED.node.config({});
+    KeyboardBlock(RED);
+    RED.node.get().emit('input', msg);
+
+    return RED.node.get().await()
+      .then(function () {
+        assert.equal(RED.node.message().payload.type, 'buttons');
+        assert.equal(RED.node.message().payload.content, 'message for the buttons');
+        assert.equal(RED.node.message().payload.chatId, 42);
+        assert.isArray(RED.node.message().payload.buttons);
+        assert.equal(RED.node.message().payload.buttons[0].value, 'value 1');
+        assert.equal(RED.node.message().payload.buttons[0].label, 'Value 1');
+        assert.equal(RED.node.message().payload.buttons[1].value, 'value 2');
+        assert.equal(RED.node.message().payload.buttons[1].label, 'Value 2');
+        assert.equal(RED.node.message().payload.buttons[2].value, 'value 3');
+        assert.equal(RED.node.message().payload.buttons[2].label, 'Value 3');
+      });
 
   });
 
