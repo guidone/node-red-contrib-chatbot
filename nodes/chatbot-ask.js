545638be9daeb0cfa1082b7a2cc73466bb23ac6f
Guidone <guido.bellomo@gmail.com>
2018-02-14 16:42:34 +0100

    fix tests

--- nodes/chatbot-ask.js
+++ nodes/chatbot-ask.js
@@ -8,39 +8,23 @@ module.exports = function(RED) {
   function ChatBotAsk(config) {
     RED.nodes.createNode(this, config);
     var node = this;
-    this.answers = config.answers;
+    this.buttons = config.answers; // keep for retrocompatibility
     this.message = config.message;
     this.inline = config.inline;
     this.transports = ['telegram'];
 
     this.on('input', function(msg) {
-
       var chatId = utils.getChatId(msg);
       var messageId = utils.getMessageId(msg);
       var template = MessageTemplate(msg, node);
-      //var answers = null;
-      //var message = null;
 
       // check transport compatibility
       if (!utils.matchTransport(node, msg)) {
         return;
       }
 
-      var answers = utils.extractValue('arrayOfString', 'buttons', node, msg, true);
+      var buttons = utils.extractValue('arrayOfObject', 'buttons', node, msg, true);
       var message = utils.extractValue('string', 'message', node, msg, false);
-
-      // get from config or payload
-      /*if (_.isArray(node.answers) && !_.isEmpty(node.answers)) {
-        answers = node.answers;
-      } else if (_.isObject(msg.payload) && _.isArray(msg.payload.buttons) && !_.isEmpty(msg.payload.buttons)) {
-        answers = msg.payload.buttons;
-      }
-      // get from config or payload
-      if (_.isString(node.message) && !_.isEmpty(node.message)) {
-        message = node.message;
-      } else if (_.isObject(msg.payload) && _.isString(msg.payload.message) && !_.isEmpty(msg.payload.message)) {
-        message = msg.payload.message;
-      }*/
 
       template(message)
         .then(function(translated) {
@@ -49,8 +33,9 @@ module.exports = function(RED) {
             content: message != null ? emoji.emojify(translated) : null,
             chatId: chatId,
             messageId: messageId,
-            buttons: answers
+            buttons: buttons
           };
+
           node.send(msg);
         });
     });
