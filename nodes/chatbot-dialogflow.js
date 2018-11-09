681b304817f67fd8eaa8425764da93dd9db59a80
Guidone <guido.bellomo@gmail.com>
2018-11-09 14:40:00 +0100

    upgraded dialogflow to v2

--- nodes/chatbot-dialogflow.js
+++ nodes/chatbot-dialogflow.js
@@ -1,6 +1,8 @@
 var _ = require('underscore');
 var utils = require('../lib/helpers/utils');
 var lcd = require('../lib/helpers/lcd');
+var dialogflow = require('dialogflow');
+
 
 var when = utils.when;
 
@@ -23,9 +25,10 @@ module.exports = function(RED) {
       var debug = utils.extractValue('boolean', 'debug', node, msg, false);
       var chatId = utils.getChatId(msg);
 
+
       // exit if empty credentials
-      if (dialogFlowNode == null || dialogFlowNode.credentials == null || _.isEmpty(dialogFlowNode.credentials.token)) {
-        lcd.warn('Dialogflow.ai token is missing.');
+      if (dialogFlowNode == null || dialogFlowNode.credentials == null) {
+        lcd.warn('Dialogflow.ai credentials are missing.');
         return;
       }
       // error if no language at all
@@ -40,21 +43,61 @@ module.exports = function(RED) {
         return;
       }
 
-      var requestUrl = 'https://api.dialogflow.com/v1/query?'
-        + 'v=20170712'
-        + '&query=' + encodeURIComponent(msg.payload.content)
-        + '&lang=' + language.toLowerCase()
-        + '&resetContexts=true'
-        + '&sessionId=' + chatId
-        + '&timezone=CET';
-      var dialogFlow = {
-        method: 'GET',
-        url: requestUrl,
-        json: true,
-        headers: {
-          'Authorization': 'Bearer ' + dialogFlowNode.credentials.token
+      var email = dialogFlowNode.credentials.email;
+      var privateKey = dialogFlowNode.credentials.privateKey;
+      var projectId = dialogFlowNode.credentials.projectId;
+
+
+
+      var sessionClient = new dialogflow.SessionsClient({
+        credentials: {
+          //private_key: private_key,
+          //client_email: 'dialogflow-mdnmfu@guidone-6ce99.iam.gserviceaccount.com'
+          private_key: privateKey,
+          client_email: email
+        }
+      });
+
+      /*
+      POST https://language.googleapis.com/v1/documents:analyzeEntities?key=API_KEY
+      */
+
+      console.log('projectid', projectId);
+      console.log('chatId', chatId);
+      var sessionPath = sessionClient.sessionPath(projectId, String(chatId));
+
+      var request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            text: msg.payload.content,
+            languageCode: language.toLowerCase()
+          }
         }
       };
+
+
+
+
+
+
+
+
+
+      /*sessionClient
+        .detectIntent(request)
+        .then(function(responses) {
+
+          console.log('RESP', responses[0].queryResult.intent);
+          console.log('RESP', responses[0].queryResult.parameters);
+
+
+        })
+        .catch(function(error) {
+          console.log('sbaglio', error);
+        })*/
+
+
       var intent = null;
       var variables = null;
       var answer = null;
@@ -62,15 +105,21 @@ module.exports = function(RED) {
 
       when(chatContext.set('pending', true))
         .then(function() {
-          return utils.request(dialogFlow);
+          return sessionClient.detectIntent(request);
         })
+        /*.then(function() {
+          return utils.request(dialogFlow);
+        })*/
         .then(function(response) {
           body = response;
           return when(chatContext.set('pending', false));
         })
-        .then(
-          function() {
-          if (body != null && body.result != null && body.result.metadata != null && body.result.metadata.intentName != null) {
+        .then(function() {
+
+          console.log('RESP', body[0].queryResult.intent);
+          console.log('RESP', body[0].queryResult.parameters);
+
+          /*if (body != null && body.result != null && body.result.metadata != null && body.result.metadata.intentName != null) {
             // test if no match
             if (body.result.action === 'input.unknown') {
               if (debug) {
@@ -93,7 +142,9 @@ module.exports = function(RED) {
             }
           } else {
             return Promise.reject('Error on api.dialogflow.com');
-          }
+          }*/
+
+
         })
         .then(function() {
           msg.payload = {
@@ -102,7 +153,7 @@ module.exports = function(RED) {
             answer: answer
           };
           if (debug) {
-            lcd.node(msg.payload, { node: node, title: 'Dialogflow.ai' });
+            lcd.node(msg.payload, { node: node, title: 'Dialogflow.com' });
           }
           node.send([msg, null]);
         })
@@ -122,7 +173,13 @@ module.exports = function(RED) {
 
   RED.nodes.registerType('chatbot-dialogflow-token', DialogflowToken, {
     credentials: {
-      token: {
+      email: {
+        type: 'text'
+      },
+      privateKey: {
+        type: 'text'
+      },
+      projectId: {
         type: 'text'
       }
     }
