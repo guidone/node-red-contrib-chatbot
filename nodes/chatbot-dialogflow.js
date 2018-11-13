4f1db9407e92a01d75a371fb2f42998f4eda9fe7
Guidone <guido.bellomo@gmail.com>
2018-11-13 20:51:56 +0100

    Dialogflow v2 (#261)
    * upgraded dialogflow to v2
    
    * cleanup
    
    * docs

--- nodes/chatbot-dialogflow.js
+++ nodes/chatbot-dialogflow.js
@@ -1,7 +1,7 @@
 var _ = require('underscore');
 var utils = require('../lib/helpers/utils');
 var lcd = require('../lib/helpers/lcd');
-
+var dialogflow = require('dialogflow');
 var when = utils.when;
 
 module.exports = function(RED) {
@@ -15,7 +15,6 @@ module.exports = function(RED) {
     node.variable = config.variable;
 
     this.on('input', function (msg) {
-
       var chatContext = msg.chat();
       var dialogFlowNode = RED.nodes.getNode(node.dialogflow);
       var language = utils.extractValue('string', 'language', node, msg, false);
@@ -24,8 +23,8 @@ module.exports = function(RED) {
       var chatId = utils.getChatId(msg);
 
       // exit if empty credentials
-      if (dialogFlowNode == null || dialogFlowNode.credentials == null || _.isEmpty(dialogFlowNode.credentials.token)) {
-        lcd.warn('Dialogflow.ai token is missing.');
+      if (dialogFlowNode == null || dialogFlowNode.credentials == null) {
+        lcd.warn('Dialogflow.ai credentials are missing.');
         return;
       }
       // error if no language at all
@@ -40,69 +39,81 @@ module.exports = function(RED) {
         return;
       }
 
-      var requestUrl = 'https://api.dialogflow.com/v1/query?'
-        + 'v=20170712'
-        + '&query=' + encodeURIComponent(msg.payload.content)
-        + '&lang=' + language.toLowerCase()
-        + '&resetContexts=true'
-        + '&sessionId=' + chatId
-        + '&timezone=CET';
-      var dialogFlow = {
-        method: 'GET',
-        url: requestUrl,
-        json: true,
-        headers: {
-          'Authorization': 'Bearer ' + dialogFlowNode.credentials.token
+      var email = dialogFlowNode.credentials.email;
+      var privateKey = dialogFlowNode.credentials.privateKey;
+      var projectId = dialogFlowNode.credentials.projectId;
+
+      var sessionClient = new dialogflow.SessionsClient({
+        credentials: {
+          private_key: privateKey,
+          client_email: email
+        }
+      });
+
+      /*
+      POST https://language.googleapis.com/v1/documents:analyzeEntities?key=API_KEY
+      */
+      var sessionPath = sessionClient.sessionPath(projectId, String(chatId));
+      var request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            text: msg.payload.content,
+            languageCode: language.toLowerCase()
+          }
         }
       };
+
+      var isFallback = null;
       var intent = null;
-      var variables = null;
+      var variables = {};
       var answer = null;
       var body = null;
 
       when(chatContext.set('pending', true))
         .then(function() {
-          return utils.request(dialogFlow);
+          return sessionClient.detectIntent(request);
         })
         .then(function(response) {
           body = response;
           return when(chatContext.set('pending', false));
         })
-        .then(
-          function() {
-          if (body != null && body.result != null && body.result.metadata != null && body.result.metadata.intentName != null) {
-            // test if no match
-            if (body.result.action === 'input.unknown') {
-              if (debug) {
-                lcd.node(body.result.action, {node: node, title: 'Dialogflow.ai'});
-              }
-              // if didn't matched any intent
-              node.send([null, msg]);
-              return Promise.reject();
-            } else {
-              intent = body.result.metadata.intentName;
-              variables = body.result.parameters;
-              answer = body.result.fulfillment != null ? body.result.fulfillment.speech : null;
-              // remove empty vars
-              _(variables).each(function(value, key) {
-                if (value == null) {
-                  delete variables[key];
-                }
-              });
-              return when(!_.isEmpty(variable) ? chatContext.set(variable, intent) : true);
-            }
-          } else {
+        .then(function() {
+          // extract variables
+          if (body == null || !_.isArray(body) || _.isEmpty(body)) {
             return Promise.reject('Error on api.dialogflow.com');
+          }
+          // parse answer
+          intent = body[0].queryResult.intent != null && body[0].queryResult.intent.displayName ?
+            body[0].queryResult.intent.displayName : null;
+          isFallback = body[0].queryResult.intent != null && body[0].queryResult.intent.isFallback ?
+            body[0].queryResult.intent.isFallback : null;
+          answer = !_.isEmpty(body[0].queryResult.fulfillmentText) ? body[0].queryResult.fulfillmentText : null;
+          // get fields
+          if (body[0].queryResult.parameters != null && body[0].queryResult.parameters.fields) {
+            _(body[0].queryResult.parameters.fields).each(function(value, key) {
+              variables[key] = value.stringValue;
+            });
+          }
+          // if empty, exit from second output and stop here
+          if (intent == null) {
+            // if didn't matched any intent
+            node.send([null, msg]);
+            return Promise.reject();
+          } else {
+            // if found, store the intent in the context and go on
+            return when(!_.isEmpty(variable) ? chatContext.set(variable, intent) : true);
           }
         })
         .then(function() {
           msg.payload = {
+            isFallback: isFallback,
             intent: intent,
-            variables: variables,
+            variables: !_.isEmpty(variables) ? variables : null,
             answer: answer
           };
           if (debug) {
-            lcd.node(msg.payload, { node: node, title: 'Dialogflow.ai' });
+            lcd.node(msg.payload, { node: node, title: 'Dialogflow.com' });
           }
           node.send([msg, null]);
         })
@@ -122,7 +133,13 @@ module.exports = function(RED) {
 
   RED.nodes.registerType('chatbot-dialogflow-token', DialogflowToken, {
     credentials: {
-      token: {
+      email: {
+        type: 'text'
+      },
+      privateKey: {
+        type: 'text'
+      },
+      projectId: {
         type: 'text'
       }
     }
