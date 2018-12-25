7b8887ee51377ba17fef187715e3e9041f9b1d59
Guidone <guido.bellomo@gmail.com>
2018-12-25 11:57:43 +0100

    add docs and remove requirement for twilio params

--- bin/generate-docs.js
+++ bin/generate-docs.js
@@ -45,6 +45,8 @@ var mappings = {
   'Slack-Receiver-node.md': 'chatbot-slack-receive.html|chatbot-slack-node',
   'Telegram-Receiver-node.md': 'chatbot-telegram-receive.html|chatbot-telegram-node',
   'Facebook-Receiver-node.md': 'chatbot-facebook-receive.html|chatbot-facebook-node',
+  'Alexa-Receiver-node.md': 'chatbot-alexa-receive.html|chatbot-alexa-node',
+  'Twilio-Receiver-node.md': 'chatbot-twilio-receive.html|chatbot-twilio-node',
   'Viber-Receiver-node.md': 'chatbot-viber-receive.html|chatbot-viber-node',
   'Switch-node.md': 'chatbot-rules.html',
   'Inline-Query-node.md': 'chatbot-inline-query.html',
