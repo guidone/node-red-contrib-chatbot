659659cbacb1be7141a8366e4ab792a859638636
Guidone <guido.bellomo@gmail.com>
2019-06-13 16:08:33 +0200

    fetch instance of context providers by node id

--- nodes/chatbot-conversation.js
+++ nodes/chatbot-conversation.js
@@ -1,5 +1,6 @@
 const utils = require('../lib/helpers/utils');
 const RegisterType = require('../lib/node-installer');
+const { UniversalPlatform, ContextProviders } = require('chat-platform');
 
 module.exports = function(RED) {
   const registerType = RegisterType(RED);
@@ -23,18 +24,27 @@ module.exports = function(RED) {
 
       const botNode = global.environment === 'production' ? node.botProduction : node.botDevelopment;
 
-      let platformNode = null;
+      let platformNode;
       if (RED.nodes.getNode(botNode) != null) {
         platformNode = RED.nodes.getNode(botNode).chat;
+      } else {
+        const contextProvider = ContextProviders.getProviderById(this.store);
+        if (contextProvider == null) {
+          node.error('Unable to find a valid chat context instance for the selected context provider');
+          return;
+        }
+        platformNode = UniversalPlatform.createServer({ contextProvider });
       }
+
       // check if chat is null, perhaps the node exists but it's not used by any receiver
       if (platformNode == null) {
         node.error('No active chatbot for this configuration. Means that the configuration was found but no receiver node is using it');
         return;
       }
-      //let message = null;
+      // finally send
       platformNode.createMessage(chatId, userId, null, msg)
         .then(message => node.send(message));
+
     });
   }
 
