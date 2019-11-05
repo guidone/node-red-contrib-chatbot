3ab7c2e1f47d899d0612d56f6c66571df85d3d7d
Guidone <guido.bellomo@gmail.com>
2019-11-05 13:29:26 +0100

    Slack blocks (#346)
    * fix global configuration
    
    * slack blocks
    
    * cleanup
    
    * tests for blocks
    
    * text context
    
    * fix broken docs
    
    * fix icon
    
    * fix response name
    
    * name of the block
    
    * lint

--- __tests__/context-node.js
+++ __tests__/context-node.js
@@ -1,12 +1,12 @@
-var _ = require('underscore');
-var assert = require('chai').assert;
-var RED = require('../lib/red-stub')();
-var ContextBlock = require('../nodes/chatbot-context');
+const _ = require('underscore');
+const assert = require('chai').assert;
+const RED = require('../lib/red-stub')();
+const ContextBlock = require('../nodes/chatbot-context');
 
-describe('Chat context node', function() {
+describe('Chat context node', () => {
 
-  it('should get a context value', function () {
-    var msg = RED.createMessage({content: 'I am a useless message'});
+  it('should get a context value', () => {
+    const msg = RED.createMessage({content: 'I am a useless message'});
     RED.node.config({
       command: 'get',
       fieldName: 'chatId'
@@ -18,14 +18,14 @@ describe('Chat context node', function() {
     ContextBlock(RED);
     RED.node.get().emit('input', msg);
     return RED.node.get().await()
-      .then(function () {
+      .then(() => {
         assert.equal(RED.node.message().payload, 42);
         assert.equal(RED.node.message().originalMessage.chat.id, 42);
       });
   });
 
-  it('should delete a context value', function () {
-    var msg = RED.createMessage({content: 'I am a useless message'});
+  it('should delete a context value', () => {
+    const msg = RED.createMessage({content: 'I am a useless message'});
     RED.node.config({
       command: 'delete',
       fieldName: 'chatId'
@@ -37,15 +37,15 @@ describe('Chat context node', function() {
     ContextBlock(RED);
     RED.node.get().emit('input', msg);
     return RED.node.get().await()
-      .then(function () {
+      .then(() => {
         assert.equal(RED.node.message().payload.content, 'I am a useless message');
         assert.equal(RED.node.message().originalMessage.chat.id, 42);
         assert.equal(msg.chat().get('chatId'), undefined);
       });
   });
 
-  it('should set a context value string', function () {
-    var msg = RED.createMessage({content: 'I am a useless message'});
+  it('should set a context value string', () => {
+    const msg = RED.createMessage({content: 'I am a useless message'});
     RED.node.config({
       command: 'set',
       fieldName: 'myValue',
@@ -59,15 +59,15 @@ describe('Chat context node', function() {
     ContextBlock(RED);
     RED.node.get().emit('input', msg);
     return RED.node.get().await()
-      .then(function () {
+      .then(() => {
         assert.equal(RED.node.message().payload.content, 'I am a useless message');
         assert.equal(RED.node.message().originalMessage.chat.id, 42);
         assert.equal(msg.chat().get('myValue'), 'I am a string value');
       });
   });
 
-  it('should set a context number string', function () {
-    var msg = RED.createMessage({content: 'I am a useless message'});
+  it('should set a context number string', () => {
+    const msg = RED.createMessage({content: 'I am a useless message'});
     RED.node.config({
       command: 'set',
       fieldName: 'myValue',
@@ -81,15 +81,15 @@ describe('Chat context node', function() {
     ContextBlock(RED);
     RED.node.get().emit('input', msg);
     return RED.node.get().await()
-      .then(function () {
+      .then(() => {
         assert.equal(RED.node.message().payload.content, 'I am a useless message');
         assert.equal(RED.node.message().originalMessage.chat.id, 42);
         assert.equal(msg.chat().get('myValue'), 4242);
       });
   });
 
-  it('should set a context boolean string', function () {
-    var msg = RED.createMessage({content: 'I am a useless message'});
+  it('should set a context boolean string', () => {
+    const msg = RED.createMessage({content: 'I am a useless message'});
     RED.node.config({
       command: 'set',
       fieldName: 'myValue',
@@ -103,15 +103,15 @@ describe('Chat context node', function() {
     ContextBlock(RED);
     RED.node.get().emit('input', msg);
     return RED.node.get().await()
-      .then(function () {
+      .then(() => {
         assert.equal(RED.node.message().payload.content, 'I am a useless message');
         assert.equal(RED.node.message().originalMessage.chat.id, 42);
         assert.equal(msg.chat().get('myValue'), true);
       });
   });
 
-  it('should set a context json string', function () {
-    var msg = RED.createMessage({content: 'I am a useless message'});
+  it('should set a context json string', () => {
+    const msg = RED.createMessage({content: 'I am a useless message'});
     RED.node.config({
       command: 'set',
       fieldName: 'myValue',
@@ -125,7 +125,7 @@ describe('Chat context node', function() {
     ContextBlock(RED);
     RED.node.get().emit('input', msg);
     return RED.node.get().await()
-      .then(function () {
+      .then(() => {
         assert.equal(RED.node.message().payload.content, 'I am a useless message');
         assert.equal(RED.node.message().originalMessage.chat.id, 42);
         assert.equal(msg.chat().get('myValue').key_1, 42);
@@ -133,8 +133,8 @@ describe('Chat context node', function() {
       });
   });
 
-  it('should set variable of an intent', function () {
-    var msg = RED.createMessage({
+  it('should set variable of an intent', () => {
+    const msg = RED.createMessage({
       type: 'intent',
       variables: {
         variable1: 'one',
@@ -148,12 +148,39 @@ describe('Chat context node', function() {
     ContextBlock(RED);
     RED.node.get().emit('input', msg);
     return RED.node.get().await()
-      .then(function () {
+      .then(() => {
         assert.equal(RED.node.message().payload.type, 'intent');
         assert.isObject(RED.node.message().payload.variables);
         assert.equal(RED.node.message().payload.variables.variable1, 'one');
         assert.equal(RED.node.message().payload.variables.variable2, 42);
         assert.equal(RED.node.message().payload.variables.variable3, true);
+        assert.equal(msg.chat().get('variable1'), 'one');
+        assert.equal(msg.chat().get('variable2'), 42);
+        assert.equal(msg.chat().get('variable3'), true);
+      });
+  });
+
+  it('should set variable from a slack response', () => {
+    const msg = RED.createMessage({
+      type: 'response',
+      content: {
+        variable1: 'one',
+        variable2: 42,
+        variable3: true
+      }
+    });
+    RED.node.config({
+      command: 'intent'
+    });
+    ContextBlock(RED);
+    RED.node.get().emit('input', msg);
+    return RED.node.get().await()
+      .then(() => {
+        assert.equal(RED.node.message().payload.type, 'response');
+        assert.isObject(RED.node.message().payload.content);
+        assert.equal(RED.node.message().payload.content.variable1, 'one');
+        assert.equal(RED.node.message().payload.content.variable2, 42);
+        assert.equal(RED.node.message().payload.content.variable3, true);
         assert.equal(msg.chat().get('variable1'), 'one');
         assert.equal(msg.chat().get('variable2'), 42);
         assert.equal(msg.chat().get('variable3'), true);
---
+++ nodes/chatbot-slack-blocks.js
@@ -0,0 +1,70 @@
+const RegisterType = require('../lib/node-installer');
+const _ = require('underscore');
+const { ChatExpress } = require('chat-platform');
+const { 
+  isValidMessage, 
+  getChatId, 
+  getMessageId, 
+  getTransport, 
+  extractValue 
+} = require('../lib/helpers/utils');
+const MessageTemplate = require('../lib/message-template-async');
+
+require('../lib/platforms/telegram');
+require('../lib/platforms/slack');
+
+module.exports = function(RED) {
+  const registerType = RegisterType(RED);
+
+  function ChatBotSlackBlocks(config) {    
+    RED.nodes.createNode(this, config);
+    const node = this;
+    this.blocks = config.blocks;
+
+    this.on('input', function(msg, send, done) {
+      // send/done compatibility for node-red < 1.0
+      send = send || function() { node.send.apply(node, arguments) };
+      done = done || function(error) { node.error.call(node, error, msg) };
+      // check if valid message
+      if (!isValidMessage(msg, node)) {
+        return;
+      }
+      const chatId = getChatId(msg);
+      const messageId = getMessageId(msg);
+      const template = MessageTemplate(msg, node);
+      const transport = getTransport(msg);   
+      // check transport compatibility
+      if (!ChatExpress.isSupported(transport, 'blocks')) {
+        done(`Node "blocks" is not supported by ${transport} transport`);
+        return;
+      }
+      // extract vars
+      let blocks = extractValue('string', 'blocks', node, msg);
+      // parse blocks
+      let rawBlocks;
+      try {
+        rawBlocks = JSON.parse(blocks);
+      } catch(e) {
+        done(e);
+        return;
+      }
+
+      template(rawBlocks)
+        .then(renderedBlocks => {
+          send({ 
+            ...msg, 
+            payload: {
+              type: 'blocks',
+              content: _.isObject(renderedBlocks) && renderedBlocks.blocks != null ? renderedBlocks.blocks : renderedBlocks,
+              chatId: chatId,
+              messageId: messageId,
+              inbound: false
+            }
+          });
+          done();
+        });
+    });
+  }
+  
+  registerType('chatbot-slack-blocks', ChatBotSlackBlocks);
+};
