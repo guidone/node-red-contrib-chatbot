96f6daf1bed549e8e8d5d2feab2c7889890b3d8a
Fazli Sapuan <fazli@sapuan.org>
2019-11-13 16:30:28 +0800

    Add support for dialogflow list parameters (#338)

--- nodes/chatbot-dialogflow.js
+++ nodes/chatbot-dialogflow.js
@@ -5,24 +5,30 @@ const dialogflow = require('dialogflow');
 const when = utils.when;
 const RegisterType = require('../lib/node-installer');
 
+function parseValue(value) {
+  if (value.kind === 'stringValue') {
+    if (!_.isEmpty(value.stringValue)) {
+      return value.stringValue;
+    }
+  } else if (value.kind === 'numberValue') {
+    return value.numberValue;
+  } else if (value.kind === 'structValue' ) {
+    return parseFields(value.structValue.fields);
+  } else if (value.kind === 'listValue') {
+    return value.listValue.values.map(parseValue);
+  } else {
+    // eslint-disable-next-line no-console
+    console.log('Warning: incorrectly handled dialogflow.com response type:');
+    // eslint-disable-next-line no-console
+    console.log(value);
+  }
+}
+
 function parseFields(fields) {
   var variables = {};
 
   _(fields).each(function(value, key) {
-    if (value.kind === 'stringValue') {
-      if (!_.isEmpty(value.stringValue)) {
-        variables[key] = value.stringValue;
-      }
-    } else if (value.kind === 'numberValue') {
-      variables[key] = value.numberValue;
-    } else if (value.kind === 'structValue' ) {
-      variables[key] = parseFields(value.structValue.fields);
-    } else {
-      // eslint-disable-next-line no-console
-      console.log('Warning: incorrectly handled dialogflow.com response type:');
-      // eslint-disable-next-line no-console
-      console.log(value);
-    }
+    variables[key] = parseValue(value);
   });
 
   return variables;
