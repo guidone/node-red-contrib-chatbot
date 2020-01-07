9c89ca433c8a47289fd78260428c4a0ab42fe8c5
Guidone <guido.bellomo@gmail.com>
2020-01-07 16:20:22 +0100

    fix telegram webhook for postbacks (#377)
    * fix telegram webhook for postbacks
    
    * remove mandatory message
    
    * upgrade to ndoe-red 1.0
    
    * fix missing underscore
    
    * test for quick replies
    
    * cleanup

--- nodes/chatbot-quick-replies.js
+++ nodes/chatbot-quick-replies.js
@@ -1,7 +1,14 @@
 const MessageTemplate = require('../lib/message-template-async');
 const emoji = require('node-emoji');
-const utils = require('../lib/helpers/utils');
 const RegisterType = require('../lib/node-installer');
+const { ChatExpress } = require('chat-platform');
+const { 
+  isValidMessage, 
+  getChatId, 
+  getMessageId, 
+  getTransport, 
+  extractValue 
+} = require('../lib/helpers/utils');
 
 module.exports = function(RED) {
   const registerType = RegisterType(RED);
@@ -13,32 +20,43 @@ module.exports = function(RED) {
     this.message = config.message;
     this.transports = ['facebook'];
 
-    this.on('input', function(msg) {
-
+    this.on('input', function(msg, send, done) {
+      // send/done compatibility for node-red < 1.0
+      send = send || function() { node.send.apply(node, arguments) };
+      done = done || function(error) { node.error.call(node, error, msg) };
+      // check if valid message
+      if (!isValidMessage(msg, node)) {
+        done('Invalid input message');
+        return;
+      }      
+      // get RedBot values
+      const chatId = getChatId(msg);
+      const messageId = getMessageId(msg);
+      const template = MessageTemplate(msg, node);
+      const transport = getTransport(msg);      
       // check transport compatibility
-      if (!utils.matchTransport(node, msg)) {
+      if (!ChatExpress.isSupported(transport, 'quick-replies')) {
+        done(`Node "quick-replies" is not supported by ${transport} transport`);
         return;
       }
-
-      var chatId = utils.getChatId(msg);
-      var messageId = utils.getMessageId(msg);
-      var template = MessageTemplate(msg, node);
-
       // get values from config
       // prepare the message, first the config, then payload
-      var buttons = utils.extractValue('buttons', 'buttons', node, msg);
-      var message = utils.extractValue('string', 'message', node, msg);
+      const buttons = extractValue('buttons', 'buttons', node, msg);
+      const message = extractValue('string', 'message', node, msg);
 
-      template(message)
-        .then(function(translated) {
-          msg.payload = {
-            type: 'quick-replies',
-            content: message != null ? emoji.emojify(translated) : null,
-            chatId: chatId,
-            messageId: messageId,
-            buttons: buttons
-          };
-          node.send(msg);
+      template(message, buttons)
+        .then(([translatedMessage, translatedButtons]) => {        
+          send({
+            ...msg,
+            payload: {
+              type: 'quick-replies',
+              content: message != null ? emoji.emojify(translatedMessage) : null,
+              chatId: chatId,
+              messageId: messageId,
+              buttons: translatedButtons
+            }
+          });
+          done();
         });
     });
   }
