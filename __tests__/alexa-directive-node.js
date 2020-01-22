bf8211d43cbfe6db4c0e24312e953f4300881da0
Guido <guido.bellomo@gmail.com>
2020-01-22 17:52:00 +0100

    fix tests

--- __tests__/alexa-directive-node.js
+++ __tests__/alexa-directive-node.js
@@ -1,12 +1,14 @@
-var _ = require('underscore');
-var assert = require('chai').assert;
-var RED = require('../lib/red-stub')();
-var AlexaDirectiveBlock = require('../nodes/chatbot-alexa-directive');
+const _ = require('underscore');
+const assert = require('chai').assert;
+const RED = require('../lib/red-stub')();
+const AlexaDirectiveBlock = require('../nodes/chatbot-alexa-directive');
 
-describe('Chat alexa directive node', function() {
+require('../lib/platforms/alexa');
 
-  it('should set a directive to delegate', function() {
-    var msg = RED.createMessage(null, 'alexa');
+describe('Chat alexa directive node', () => {
+
+  it('should set a directive to delegate', () => {
+    const msg = RED.createMessage(null, 'alexa');
     RED.node.config({
       directiveType: 'Dialog.Delegate'
     });
@@ -14,14 +16,14 @@ describe('Chat alexa directive node', function() {
     AlexaDirectiveBlock(RED);
     RED.node.get().emit('input', msg);
     return RED.node.get().await()
-      .then(function() {
+      .then(() => {
         assert.equal(RED.node.message().payload.type, 'directive');
         assert.equal(RED.node.message().payload.directiveType, 'Dialog.Delegate');
       });
   });
 
-  it('should elicit a slot', function() {
-    var msg = RED.createMessage(null, 'alexa');
+  it('should elicit a slot', () => {
+    const msg = RED.createMessage(null, 'alexa');
     RED.node.config({
       directiveType: 'Dialog.ElicitSlot',
       slot: 'a-slot'
@@ -30,15 +32,15 @@ describe('Chat alexa directive node', function() {
     AlexaDirectiveBlock(RED);
     RED.node.get().emit('input', msg);
     return RED.node.get().await()
-      .then(function() {
+      .then(() => {
         assert.equal(RED.node.message().payload.type, 'directive');
         assert.equal(RED.node.message().payload.directiveType, 'Dialog.ElicitSlot');
         assert.equal(RED.node.message().payload.slotToElicit, 'a-slot');
       });
   });
 
-  it('should elicit a slot from payload', function() {
-    var msg = RED.createMessage({
+  it('should elicit a slot from payload', () => {
+    const msg = RED.createMessage({
       directiveType: 'Dialog.ElicitSlot',
       slot: 'a-slot'
     }, 'alexa');
@@ -47,15 +49,15 @@ describe('Chat alexa directive node', function() {
     AlexaDirectiveBlock(RED);
     RED.node.get().emit('input', msg);
     return RED.node.get().await()
-      .then(function() {
+      .then(() => {
         assert.equal(RED.node.message().payload.type, 'directive');
         assert.equal(RED.node.message().payload.directiveType, 'Dialog.ElicitSlot');
         assert.equal(RED.node.message().payload.slotToElicit, 'a-slot');
       });
   });
 
-  it('should confirm a slot', function() {
-    var msg = RED.createMessage(null, 'alexa');
+  it('should confirm a slot', () => {
+    const msg = RED.createMessage(null, 'alexa');
     RED.node.config({
       directiveType: 'Dialog.ConfirmSlot',
       slot: 'a-slot'
@@ -64,14 +66,12 @@ describe('Chat alexa directive node', function() {
     AlexaDirectiveBlock(RED);
     RED.node.get().emit('input', msg);
     return RED.node.get().await()
-      .then(function() {
+      .then(() => {
         assert.equal(RED.node.message().payload.type, 'directive');
         assert.equal(RED.node.message().payload.directiveType, 'Dialog.ConfirmSlot');
         assert.equal(RED.node.message().payload.slotToConfirm, 'a-slot');
       });
   });
-
-
 
 });
 
