ecd9ce276cddb9962da93bc76f5922635bbcf2e2
Guidone <guido.bellomo@gmail.com>
2020-01-31 11:36:45 -0600

    fix text language

--- __tests__/language-node.js
+++ __tests__/language-node.js
@@ -1,72 +1,61 @@
-var _ = require('underscore');
-var assert = require('chai').assert;
-var RED = require('../lib/red-stub')();
-var LanguageBlock = require('../nodes/chatbot-language');
+const _ = require('underscore');
+const assert = require('chai').assert;
+const RED = require('../lib/red-stub')();
+const LanguageBlock = require('../nodes/chatbot-language');
 
-describe('Chat language node', function() {
+describe('Chat language node', () => {
 
-  it('should detect italian language', function() {
-    var msg = RED.createMessage({
-      content: 'hey guido come stai?',
+  it('should detect italian language and override it', () => {
+    const msg = RED.createMessage({
+      content: 'posso avere un gelato?',
       chatId: 42
     });
+    msg.chat().set('language', 'en');
     RED.node.config({
       language: 'italian',
       mode: 'medium'
     });
     LanguageBlock(RED);
     RED.node.get().emit('input', msg);
-    assert.equal(RED.node.message().payload.content, 'hey guido come stai?');
-    assert.equal(RED.node.message().payload.chatId, 42);
-    assert.equal(RED.node.message(1), null);
+    return RED.node.get().await()
+      .then(() => {
+        assert.equal(msg.chat().get('language'), 'it');
+        assert.equal(RED.node.message().payload.content, 'posso avere un gelato?');
+        assert.equal(RED.node.message().payload.chatId, 42);        
+      })    
   });
 
-  it('should not detect italian language', function() {
-    var msg = RED.createMessage({
-      content: 'hey guido come stai?',
-      chatId: 42
-    });
-    RED.node.config({
-      language: 'italian',
-      mode: 'strict'
-    });
-    LanguageBlock(RED);
-    RED.node.get().emit('input', msg);
-    assert.equal(RED.node.message(1).payload.content, 'hey guido come stai?');
-    assert.equal(RED.node.message(1).payload.chatId, 42);
-    assert.equal(RED.node.message(0), null);
-  });
-
-  it('should pass through commands', function() {
-    var msg = RED.createMessage({
+  it('should pass through commands', () => {
+    const msg = RED.createMessage({
       content: '/help',
       chatId: 42
     });
-    RED.node.config({
-      language: 'italian',
-      mode: 'strict'
-    });
+    msg.chat().set('language', 'it');
+    RED.node.config({});
     LanguageBlock(RED);
     RED.node.get().emit('input', msg);
-    assert.equal(RED.node.message(0).payload.content, '/help');
-    assert.equal(RED.node.message(0).payload.chatId, 42);
-    assert.equal(RED.node.message(1), null);
+    return RED.node.get().await()
+      .then(() => {
+        assert.equal(RED.node.message(0).payload.content, '/help');
+        assert.equal(RED.node.message(0).payload.chatId, 42);
+        assert.equal(msg.chat().get('language'), 'it');
+      });
   });
 
-  it('should detect english language', function() {
-    var msg = RED.createMessage({
-      content: 'how you doing?',
+  it('should detect english language', () => {
+    const msg = RED.createMessage({
+      content: 'What time is it?',
       chatId: 42
     });
-    RED.node.config({
-      language: 'english',
-      mode: 'medium'
-    });
+    RED.node.config({});
     LanguageBlock(RED);
     RED.node.get().emit('input', msg);
-    assert.equal(RED.node.message().payload.content, 'how you doing?');
-    assert.equal(RED.node.message().payload.chatId, 42);
-    assert.equal(RED.node.message(1), null);
+    return RED.node.get().await()
+      .then(() => {
+        assert.equal(msg.chat().get('language'), 'en');
+        assert.equal(RED.node.message().payload.content, 'What time is it?');
+        assert.equal(RED.node.message().payload.chatId, 42);
+      });
   });
 
 });
