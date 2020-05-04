ce90e85691c8cd0c1aa0eab34e02c5219dbfa0fa
Guido <guido.bellomo@gmail.com>
2020-05-04 22:46:13 +0200

    pop message node

---
+++ __tests__/pop-message-node.js
@@ -0,0 +1,30 @@
+const _ = require('underscore');
+const assert = require('chai').assert;
+const RED = require('../lib/red-stub')();
+const PopBlock = require('../nodes/chatbot-pop-message');
+
+describe('Chat message node node', () => {
+  it('set the entity payload', async () => {
+    const msg = RED.createRawMessage({
+      previous: {
+        type: 'message',
+        content: 'the previous message',
+        inbound: false
+      },
+      payload: {
+        type: 'message',
+        content: 'the current message',
+        inbound: false
+      }
+    });
+    RED.node.config({});
+    PopBlock(RED);
+    RED.node.get().emit('input', msg);
+    await RED.node.get().await()
+    const payload = RED.node.message().payload;
+    assert.equal(payload.type, 'message');
+    assert.equal(payload.content, 'the previous message');
+    assert.equal(payload.inbound, false);
+  });
+
+});
