74d4c96a7b4ba1d3ef1fc15a0e521b31f48f9952
guidone <guido.bellomo@gmail.com>
2021-09-21 23:39:39 +0200

    improve params node for chained messages

--- __tests__/params-node.js
+++ __tests__/params-node.js
@@ -5,6 +5,37 @@ const ParamsBlock = require('../nodes/chatbot-params');
 
 describe('Chat params node', () => {
 
+  it('should pass thru 1 key for slack for piled messages', function() {
+    const msg = RED.createMessage([
+      { content: 'first message' },
+      { content: 'second message' }
+    ], 'slack');
+    RED.node.config({
+      params: [
+        { platform: 'telegram', name: 'my-value1', value: 42 },
+        { platform: 'telegram', name: 'my-value2', value: true },
+        { platform: 'telegram', name: 'my-value3', value: 'just a string' },
+        { platform: 'slack', name: 'my-chatId', value: '{{chatId}}' }
+      ]
+    });
+    ParamsBlock(RED);
+    msg.chat().set({ chatId: '42' });
+    RED.node.get().emit('input', msg);
+
+    return RED.node.get().await()
+      .then(() => {
+        const message = RED.node.message(0);
+        assert.equal(message.originalMessage.chat.id, '42');
+        assert.isArray(message.payload)
+        assert.equal(message.payload[0].content, 'first message');
+        assert.lengthOf(Object.keys(message.payload[0].params), 1);
+        assert.equal(message.payload[0].params['my-chatId'], '42');
+        assert.equal(message.payload[1].content, 'second message');
+        assert.lengthOf(Object.keys(message.payload[1].params), 1);
+        assert.equal(message.payload[1].params['my-chatId'], '42');
+      });
+  });
+
   it('should pass thru 3 keys for telegram', function() {
     const msg = RED.createMessage({ content: 'a payload'}, 'telegram');
     RED.node.config({
@@ -32,7 +63,7 @@ describe('Chat params node', () => {
   });
 
   it('should pass thru 3 keys for telegram in upstream nodes', function() {
-    const msg = RED.createMessage({ 
+    const msg = RED.createMessage({
       content: 'a payload',
       params: [
         { platform: 'telegram', name: 'my-value1', value: 42 },
@@ -83,7 +114,7 @@ describe('Chat params node', () => {
   });
 
   it('should pass thru 1 key for slack in upstream node', function() {
-    const msg = RED.createMessage({ 
+    const msg = RED.createMessage({
       content: 'a payload',
       params: [
         { platform: 'telegram', name: 'my-value1', value: 42 },
@@ -131,4 +162,3 @@ describe('Chat params node', () => {
   });
 
 });
-
