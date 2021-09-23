668dd11428e19e40b8db11db44afe209fa75f609
guidone <guido.bellomo@gmail.com>
2021-09-23 17:31:02 +0200

    add telegram menu and update telegram library

---
+++ nodes/chatbot-telegram-menu.js
@@ -0,0 +1,45 @@
+const _ = require('underscore');
+const RegisterType = require('../lib/node-installer');
+
+module.exports = function(RED) {
+  const registerType = RegisterType(RED);
+
+  function ChatBotTelegramrMenu(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    this.bot = config.bot;
+    this.items = config.items;
+
+    this.on('input', async function(msg, send, done) {
+      // send/done compatibility for node-red < 1.0
+      done = done || function(error) { node.error.call(node, error, msg) };
+      // check bot
+      if (node.bot == null) {
+        done('Missing Telegram Bot configuration');
+        return;
+      }
+      const currentBot = RED.nodes.getNode(node.bot);
+      if (currentBot == null || currentBot.chat == null) {
+        done('Invalid Telegram Bot configuration');
+        return;
+      }
+      const { connector } = currentBot.chat.getOptions();
+      // get items
+      const items = node.items;
+      const validItems = Array.isArray(items) && items.every(item => _.isObject(item) && !_.isEmpty(item.command))
+      if (!validItems) {
+        done('Invalid commands, "command" parameter is required');
+        return;
+      }
+      // finally set the commands
+      const result = await connector.setMyCommands(items);
+      if (!result) {
+        done('Error setting commands in Telegram bot');
+        return;
+      }
+      done();
+    });
+  }
+
+  registerType('chatbot-telegram-menu', ChatBotTelegramrMenu);
+};
