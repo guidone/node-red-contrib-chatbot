f28b9b496ca95f8d28da6d0df3efc9b1f956f7f3
guidone <guido.bellomo@gmail.com>
2022-06-14 21:29:13 +0200

    move nodes and docs generator

---
+++ bin/nodes.js
@@ -0,0 +1,327 @@
+module.exports = [
+  {
+    markdownFile: 'Mission-Control-GraphQL.md',
+    nodeFile: 'mc-graphql.html',
+    nodeType: 'mc-graphql'
+  },
+  {
+    markdownFile: 'Generic-Template-node.md',
+    nodeFile: 'chatbot-generic-template.html',
+    nodeType: 'chatbot-generic-template'
+  },
+  {
+    markdownFile: 'Quick-Replies-node.md',
+    nodeFile: 'chatbot-quick-replies.html',
+    nodeType: 'chatbot-quick-replies'
+  },
+  {
+    markdownFile: 'Messenger-Menu-node.md',
+    nodeFile: 'chatbot-messenger-menu.html',
+    nodeType: 'chatbot-messenger-menu'
+  },
+  {
+    markdownFile: 'Messenger-Template-node.md',
+    nodeFile: 'chatbot-messenger-template.html',
+    nodeType: 'chatbot-messenger-template'
+  },
+  {
+    markdownFile: 'Document-node.md',
+    nodeFile: 'chatbot-document.html',
+    nodeType: 'chatbot-document'
+  },
+  {
+    markdownFile: 'Message-node.md',
+    nodeFile: 'chatbot-message.html',
+    nodeType: 'chatbot-message'
+  },
+  {
+    markdownFile: 'QR-Code-node.md',
+    nodeFile: 'chatbot-qrcode.html',
+    nodeType: 'chatbot-qrcode'
+  },
+  {
+    markdownFile: 'Rivescript-node.md',
+    nodeFile: 'chatbot-rivescript.html',
+    nodeType: 'chatbot-rivescript'
+  },
+  {
+    markdownFile: 'Conversation-node.md',
+    nodeFile: 'chatbot-conversation.html',
+    nodeType: 'chatbot-conversation'
+  },
+  {
+    markdownFile: 'Command-node.md',
+    nodeFile: 'chatbot-command.html',
+    nodeType: 'chatbot-command'
+  },
+  {
+    markdownFile: 'Analytics-node.md',
+    nodeFile: 'chatbot-analytics.html',
+    nodeType: 'chatbot-analytics'
+  },
+  {
+    markdownFile: 'Transport-node.md',
+    nodeFile: 'chatbot-transport.html',
+    nodeType: 'chatbot-transport'
+  },
+  {
+    markdownFile: 'Topic-node.md',
+    nodeFile: 'chatbot-topic.html',
+    nodeType: 'chatbot-topic'
+  },
+  {
+    markdownFile: 'Debug-node.md',
+    nodeFile: 'chatbot-debug.html',
+    nodeType: 'chatbot-debug'
+  },
+  {
+    markdownFile: 'Parse-node.md',
+    nodeFile: 'chatbot-parse.html',
+    nodeType: 'chatbot-parse'
+  },
+  {
+    markdownFile: 'Request-node.md',
+    nodeFile: 'chatbot-request.html',
+    nodeType: 'chatbot-request'
+  },
+  {
+    markdownFile: 'Video-node.md',
+    nodeFile: 'chatbot-video.html',
+    nodeType: 'chatbot-video'
+  },
+  {
+    markdownFile: 'Audio-node.md',
+    nodeFile: 'chatbot-audio.html',
+    nodeType: 'chatbot-audio'
+  },
+  {
+    markdownFile: 'Voice-node.md',
+    nodeFile: 'chatbot-voice.html',
+    nodeType: 'chatbot-voice'
+  },
+  {
+    markdownFile: 'Image-node.md',
+    nodeFile: 'chatbot-image.html',
+    nodeType: 'chatbot-image'
+  },
+  {
+    markdownFile: 'Language-node.md',
+    nodeFile: 'chatbot-language.html',
+    nodeType: 'chatbot-language'
+  },
+  {
+    markdownFile: 'Authorized-node.md',
+    nodeFile: 'chatbot-authorized.html',
+    nodeType: 'chatbot-authorized'
+  },
+  {
+    markdownFile: 'Log-node.md',
+    nodeFile: 'chatbot-log.html',
+    nodeType: 'chatbot-log'
+  },
+  {
+    markdownFile: 'Location-node.md',
+    nodeFile: 'chatbot-location.html',
+    nodeType: 'chatbot-location'
+  },
+  {
+    markdownFile: 'Context-node.md',
+    nodeFile: 'chatbot-context.html',
+    nodeType: 'chatbot-context'
+  },
+  {
+    markdownFile: 'Sticker-node.md',
+    nodeFile: 'chatbot-sticker.html',
+    nodeType: 'chatbot-sticker'
+  },
+  {
+    markdownFile: 'Animation-node.md',
+    nodeFile: 'chatbot-animation.html',
+    nodeType: 'chatbot-animation'
+  },
+  {
+    markdownFile: 'Telegram-Menu-node.md',
+    nodeFile: 'chatbot-telegram-menu.html',
+    nodeType: 'chatbot-telegram-menu'
+  },
+  {
+    markdownFile: 'Waiting-node.md',
+    nodeFile: 'chatbot-waiting.html',
+    nodeType: 'chatbot-waiting'
+  },
+  {
+    markdownFile: 'Slack-Receiver-node.md',
+    nodeFile: 'chatbot-slack-receive.html',
+    nodeType: 'chatbot-slack-node'
+  },
+  {
+    markdownFile: 'Telegram-Receiver-node.md',
+    nodeFile: 'chatbot-telegram-receive.html',
+    nodeType: 'chatbot-telegram-node'
+  },
+  {
+    markdownFile: 'Facebook-Receiver-node.md',
+    nodeFile: 'chatbot-facebook-receive.html',
+    nodeType: 'chatbot-facebook-node'
+  },
+  {
+    markdownFile: 'Alexa-Receiver-node.md',
+    nodeFile: 'chatbot-alexa-receive.html',
+    nodeType: 'chatbot-alexa-node'
+  },
+  {
+    markdownFile: 'Twilio-Receiver-node.md',
+    nodeFile: 'chatbot-twilio-receive.html',
+    nodeType: 'chatbot-twilio-node'
+  },
+  {
+    markdownFile: 'Routee-Receiver-node.md',
+    nodeFile: 'chatbot-routee-receive.html',
+    nodeType: 'chatbot-routee-node'
+  },
+  {
+    markdownFile: 'Microsoft-Teams-Receiver-node.md',
+    nodeFile: 'chatbot-msteams-receive.html',
+    nodeType: 'chatbot-msteams-node'
+  },
+  {
+    markdownFile: 'Viber-Receiver-node.md',
+    nodeFile: 'chatbot-viber-receive.html',
+    nodeType: 'chatbot-viber-node'
+  },
+  {
+    markdownFile: 'Switch-node.md',
+    nodeFile: 'chatbot-rules.html',
+    nodeType: 'chatbot-rules'
+  },
+  {
+    markdownFile: 'Inline-Query-node.md',
+    nodeFile: 'chatbot-inline-query.html',
+    nodeType: 'chatbot-inline-query'
+  },
+  {
+    markdownFile: 'Dialog-node.md',
+    nodeFile: 'chatbot-dialog.html',
+    nodeType: 'chatbot-dialog'
+  },
+  {
+    markdownFile: 'Rules-node.md',
+    nodeFile: 'chatbot-rules.html',
+    nodeType: 'chatbot-rules'
+  },
+  {
+    markdownFile: 'Dialogflow-node.md',
+    nodeFile: 'chatbot-dialogflow.html',
+    nodeType: 'chatbot-dialogflow'
+  },
+  {
+    markdownFile: 'Dialogflow-token-node.md',
+    nodeFile: 'chatbot-dialogflow.html',
+    nodeType: 'chatbot-dialogflow-token'
+  },
+  {
+    markdownFile: 'Invoice-node.md',
+    nodeFile: 'chatbot-invoice.html',
+    nodeType: 'chatbot-invoice'
+  },
+  {
+    markdownFile: 'Invoice-Shipping-node.md',
+    nodeFile: 'chatbot-invoice-shipping.html',
+    nodeType: 'chatbot-invoice-shipping'
+  },
+  {
+    markdownFile: 'Chat-Context.md',
+    nodeFile: 'chatbot-context-store.html',
+    nodeType: 'chatbot-context-store'
+  },
+  {
+    markdownFile: 'Keyboard-node.md',
+    nodeFile: 'chatbot-ask.html',
+    nodeType: 'chatbot-ask'
+  },
+  {
+    markdownFile: 'Buttons-node.md',
+    nodeFile: 'chatbot-inline-buttons.html',
+    nodeType: 'chatbot-inline-buttons'
+  },
+  {
+    markdownFile: 'Extend-node.md',
+    nodeFile: 'chatbot-extend.html',
+    nodeType: 'chatbot-extend'
+  },
+  {
+    markdownFile: 'Broadcast-node.md',
+    nodeFile: 'chatbot-broadcast.html',
+    nodeType: 'chatbot-broadcast'
+  },
+  {
+    markdownFile: 'Support-table.md',
+    nodeFile: 'chatbot-support-table.html',
+    nodeType: 'chatbot-support-table'
+  },
+  {
+    markdownFile: 'Alexa-Card-node.md',
+    nodeFile: 'chatbot-alexa-card.html',
+    nodeType: 'chatbot-alexa-card'
+  },
+  {
+    markdownFile: 'Alexa-Speech-node.md',
+    nodeFile: 'chatbot-alexa-speech.html',
+    nodeType: 'chatbot-alexa-speech'
+  },
+  {
+    markdownFile: 'Alexa-Directive-node.md',
+    nodeFile: 'chatbot-alexa-directive.html',
+    nodeType: 'chatbot-alexa-directive'
+  },
+  {
+    markdownFile: 'Universal-Connector-node.md',
+    nodeFile: 'chatbot-universal-receive.html',
+    nodeType: 'chatbot-universal-receive'
+  },
+  {
+    markdownFile: 'Slack-blocks-node.md',
+    nodeFile: 'chatbot-slack-blocks.html',
+    nodeType: 'chatbot-slack-blocks'
+  },
+  {
+    markdownFile: 'Params-node.md',
+    nodeFile: 'chatbot-params.html',
+    nodeType: 'chatbot-params'
+  },
+  {
+    markdownFile: 'NLPjs-Train.md',
+    nodeFile: 'chatbot-nlp-train.html',
+    nodeType: 'chatbot-nlpjs-train'
+  },
+  {
+    markdownFile: 'NLPjs-Entity.md',
+    nodeFile: 'chatbot-nlp-entity.html',
+    nodeType: 'chatbot-nlpjs-entity'
+  },
+  {
+    markdownFile: 'NLPjs-Intent.md',
+    nodeFile: 'chatbot-nlp-intent.html',
+    nodeType: 'chatbot-nlpjs-intent'
+  },
+  {
+    markdownFile: 'NLPjs-Save.md',
+    nodeFile: 'chatbot-nlp-save.html',
+    nodeType: 'chatbot-nlpjs-save'
+  },
+  {
+    markdownFile: 'NLPjs-Load.md',
+    nodeFile: 'chatbot-nlp-load.html',
+    nodeType: 'chatbot-nlpjs-load'
+  },
+  {
+    markdownFile: 'NLPjs-Process.md',
+    nodeFile: 'chatbot-nlp.html',
+    nodeType: 'chatbot-nlpjs'
+  },
+  {
+    markdownFile: 'Pop-Message-node.md',
+    nodeFile: 'chatbot-pop-message.html',
+    nodeType: 'chatbot-pop-message'
+  }
+];
