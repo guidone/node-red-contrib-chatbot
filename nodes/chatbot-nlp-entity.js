a43dd140bef342b6306610decca09a1b33112971
guidone <guido.bellomo@gmail.com>
2023-01-25 17:17:34 +0100

    initial version

--- nodes/chatbot-nlp-entity.js
+++ nodes/chatbot-nlp-entity.js
@@ -15,6 +15,8 @@ module.exports = function(RED) {
     this.entities = config.entities;
     this.name = config.name;
     this.language = config.language;
+    this.entityType = config.entityType;
+    this.regex = config.regex;
 
     this.on('input', function(msg, send, done) {
       // send/done compatibility for node-red < 1.0
@@ -24,29 +26,52 @@ module.exports = function(RED) {
       const entities = utils.extractValue('arrayOfEntities', 'entities', node, msg);
       const name = utils.extractValue('string', 'name', node, msg, false);
       const language = utils.extractValue('string', 'language', node, msg, false);
+      const entityType = utils.extractValue('string', 'entityType', node, msg, false);
 
-      msg.payload = _.isObject(msg.payload) ? msg.payload : {}; 
-      // collect the lexicon of the node and mix with the one of the incoming payload
-      if (_.isArray(entities) && !_.isEmpty(entities)) {
+      msg.payload = _.isObject(msg.payload) ? msg.payload : {};
 
-        // make sure we replace the entities[] in the payload
-        if (!_.isObject(msg.payload.entities) || _.isArray(msg.payload.entities)) {          
-          msg.payload.entities = {};
-        }
-        if (!_.isObject(msg.payload.entities[language])) {
-          msg.payload.entities[language] = {};
-        }
-        if (!_.isObject(msg.payload.entities[language][name])) {
-          msg.payload.entities[language][name] = {};
-        }
-        // append entities
-        msg.payload.entities[language][name] = [
-          ...(_.isArray(msg.payload.entities[language][name]) ? msg.payload.entities[language][name] : []),
-          ...entities
-        ];
+      // make sure we replace the entities[] in the payload
+      if (!_.isObject(msg.payload.entities) || _.isArray(msg.payload.entities)) {
+        msg.payload.entities = {};
+      }
+      if (!_.isObject(msg.payload.entities[language])) {
+        msg.payload.entities[language] = {};
+      }
+      if (!_.isObject(msg.payload.entities[language][name])) {
+        msg.payload.entities[language][name] = {};
       }
 
-      node.send(msg);
+      // collect the lexicon of the node and mix with the one of the incoming payload
+      if (entityType === 'regex') {
+
+        // TODO check if valid regex
+
+        msg.payload.entities[language][name] = {
+          type: 'regex',
+          regex: node.regex
+        };
+      } else if (_.isEmpty(entityType) || entityType === 'enum') {
+
+        // TODO check if not array
+
+        if (_.isArray(entities) && !_.isEmpty(entities)) {
+          msg.payload.entities[language][name] = {
+            type: 'enum',
+            entities: [
+              ...(_.isArray(msg.payload.entities[language][name].entities) && msg.payload.entities[language][name].type === 'enum' ?
+                msg.payload.entities[language][name].entities : []),
+              ...entities
+            ],
+          };
+        }
+      } else {
+        done(`Invalid NLP.js entity type: ${entityType}`);
+        return;
+      }
+
+
+
+      send(msg);
       done();
     });
   }
