980ba7cb6a431106d333e58a2ea41d1ceec80c0b
guidone <guido.bellomo@gmail.com>
2023-01-31 20:39:19 +0100

    fix telegram keyboard

--- nodes/chatbot-ask.js
+++ nodes/chatbot-ask.js
@@ -1,9 +1,19 @@
-const utils = require('../lib/helpers/utils');
-const MessageTemplate = require('../lib/message-template-async');
 const emoji = require('node-emoji');
 const _ = require('underscore');
+const { ChatExpress } = require('chat-platform');
+
+const MessageTemplate = require('../lib/message-template-async');
 const RegisterType = require('../lib/node-installer');
 const GlobalContextHelper = require('../lib/helpers/global-context-helper');
+
+const {
+  isValidMessage,
+  getChatId,
+  getMessageId,
+  getTransport,
+  extractValue,
+  appendPayload
+} = require('../lib/helpers/utils');
 
 module.exports = function(RED) {
   const registerType = RegisterType(RED);
@@ -11,37 +21,53 @@ module.exports = function(RED) {
 
   function ChatBotAsk(config) {
     RED.nodes.createNode(this, config);
-    var node = this;
+    const node = this;
     globalContextHelper.init(this.context().global);
     this.buttons = config.buttons;
     this.message = config.message;
-    this.inline = config.inline;
-    this.transports = ['telegram'];
+    this.isPersistent = config.isPersistent;
+    this.oneTimeKeyboard = config.oneTimeKeyboard;
+    this.placeholder = config.placeholder;
 
-    this.on('input', function(msg) {
-      var chatId = utils.getChatId(msg);
-      var messageId = utils.getMessageId(msg);
-      var template = MessageTemplate(msg, node);
-
+    this.on('input', function(msg, send, done) {
+      // send/done compatibility for node-red < 1.0
+      send = send || function() { node.send.apply(node, arguments) };
+      done = done || function(error) { node.error.call(node, error, msg) };
+      const sendPayload = appendPayload(send, msg);
+      // check if valid message
+      if (!isValidMessage(msg, node)) {
+        return;
+      }
+      // get config
+      const chatId = getChatId(msg);
+      const messageId = getMessageId(msg);
+      const transport = getTransport(msg);
+      const template = MessageTemplate(msg, node);
       // check transport compatibility
-      if (!utils.matchTransport(node, msg)) {
+      if (!ChatExpress.isSupported(transport, 'telegram-buttons')) {
+        done(`Node "Buttons" is not supported by ${transport} transport`);
         return;
       }
 
-      var buttons = utils.extractValue('buttons', 'buttons', node, msg, true);
-      var message = utils.extractValue('string', 'message', node, msg, false);
+      const buttons = extractValue('buttons', 'buttons', node, msg, true);
+      const message = extractValue('string', 'message', node, msg, false);
+      const isPersistent = extractValue('boolean', 'isPersistent', node, msg, true);
+      const oneTimeKeyboard = extractValue('boolean', 'oneTimeKeyboard', node, msg, true);
+      const placeholder = extractValue('string', 'placeholder', node, msg, true);
 
       // template then send
       template(message)
         .then(function(translated) {
-          msg.payload = {
-            type: _.isEmpty(buttons) ? 'reset-buttons': 'buttons',
+          sendPayload({
+            type: _.isEmpty(buttons) ? 'telegram-reset-buttons': 'telegram-buttons',
             content: message != null ? emoji.emojify(translated) : null,
             chatId: chatId,
             messageId: messageId,
-            buttons: buttons
-          };
-          node.send(msg);
+            buttons: buttons,
+            isPersistent,
+            oneTimeKeyboard,
+            placeholder
+          });
         });
     });
   }
