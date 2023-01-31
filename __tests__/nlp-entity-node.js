17d6c14adc78b6072fb294d4f532be8c08747c72
guidone <guido.bellomo@gmail.com>
2023-01-31 22:23:24 +0100

    fix tests

--- __tests__/nlp-entity-node.js
+++ __tests__/nlp-entity-node.js
@@ -1,4 +1,3 @@
-const _ = require('underscore');
 const assert = require('chai').assert;
 const RED = require('../lib/red-stub')();
 const NLPEntityBlock = require('../nodes/chatbot-nlp-entity');
@@ -8,7 +7,7 @@ require('../lib/platforms/slack/index');
 
 describe('Chat nlp entity node', () => {
 
-  it('set the entity payload', () => {
+  it('set the entity payload, should default to enum if not specified', async () => {
     const msg = RED.createMessage();
     RED.node.config({
       name: 'fruits',
@@ -20,25 +19,58 @@ describe('Chat nlp entity node', () => {
     });
     NLPEntityBlock(RED);
     RED.node.get().emit('input', msg);
-    return RED.node.get().await()
-      .then(() => {
-        const payload = RED.node.message().payload;
-        assert.isObject(payload);
-        assert.isObject(payload.entities);
-        assert.isObject(payload.entities.en);
-        assert.isArray(payload.entities.en.fruits);
-        assert.lengthOf(payload.entities.en.fruits, 2);
-        assert.equal(payload.entities.en.fruits[0].name, 'orange');
-        assert.equal(payload.entities.en.fruits[1].name, 'apple');
-        assert.isArray(payload.entities.en.fruits[1].aliases);
-        assert.lengthOf(payload.entities.en.fruits[1].aliases, 2);
-        assert.equal(payload.entities.en.fruits[1].aliases[0], 'golden apple');
-        assert.equal(payload.entities.en.fruits[1].aliases[1], 'granny smith apple');
-      });
+    await RED.node.get().await()
 
+    const payload = RED.node.message().payload;
+    assert.isObject(payload);
+    assert.isObject(payload.entities);
+    assert.isObject(payload.entities.en);
+    assert.isObject(payload.entities.en.fruits);
+    assert.equal(payload.entities.en.fruits.type, 'enum');
+
+    const entities = payload.entities.en.fruits.entities;
+    assert.lengthOf(entities, 2);
+    assert.equal(entities[0].name, 'orange');
+    assert.equal(entities[1].name, 'apple');
+    assert.isArray(entities[1].aliases);
+    assert.lengthOf(entities[1].aliases, 2);
+    assert.equal(entities[1].aliases[0], 'golden apple');
+    assert.equal(entities[1].aliases[1], 'granny smith apple');
   });
 
-  it('set the entity payload from message', () => {
+  it('set the entity with type enum', async () => {
+    const msg = RED.createMessage();
+    RED.node.config({
+      name: 'fruits',
+      language: 'en',
+      entityType: 'enum',
+      entities: [
+        { name: 'orange' },
+        { name: 'apple', aliases: ['golden apple', 'granny smith apple'] }
+      ]
+    });
+    NLPEntityBlock(RED);
+    RED.node.get().emit('input', msg);
+    await RED.node.get().await()
+
+    const payload = RED.node.message().payload;
+    assert.isObject(payload);
+    assert.isObject(payload.entities);
+    assert.isObject(payload.entities.en);
+    assert.isObject(payload.entities.en.fruits);
+    assert.equal(payload.entities.en.fruits.type, 'enum');
+
+    const entities = payload.entities.en.fruits.entities;
+    assert.lengthOf(entities, 2);
+    assert.equal(entities[0].name, 'orange');
+    assert.equal(entities[1].name, 'apple');
+    assert.isArray(entities[1].aliases);
+    assert.lengthOf(entities[1].aliases, 2);
+    assert.equal(entities[1].aliases[0], 'golden apple');
+    assert.equal(entities[1].aliases[1], 'granny smith apple');
+  });
+
+  it('set the entity payload from message, defaults to enum', async () => {
     const msg = RED.createMessage({
       name: 'fruits',
       language: 'en',
@@ -50,31 +82,35 @@ describe('Chat nlp entity node', () => {
     RED.node.config({});
     NLPEntityBlock(RED);
     RED.node.get().emit('input', msg);
-    return RED.node.get().await()
-      .then(() => {
-        const payload = RED.node.message().payload;
-        assert.isObject(payload);
-        assert.isObject(payload.entities);
-        assert.isObject(payload.entities.en);
-        assert.isArray(payload.entities.en.fruits);
-        assert.lengthOf(payload.entities.en.fruits, 2);
-        assert.equal(payload.entities.en.fruits[0].name, 'orange');
-        assert.equal(payload.entities.en.fruits[1].name, 'apple');
-        assert.isArray(payload.entities.en.fruits[1].aliases);
-        assert.lengthOf(payload.entities.en.fruits[1].aliases, 2);
-        assert.equal(payload.entities.en.fruits[1].aliases[0], 'golden apple');
-        assert.equal(payload.entities.en.fruits[1].aliases[1], 'granny smith apple');
-      });
+    await RED.node.get().await()
 
+    const payload = RED.node.message().payload;
+    assert.isObject(payload);
+    assert.isObject(payload.entities);
+    assert.isObject(payload.entities.en);
+    assert.isObject(payload.entities.en.fruits);
+    assert.equal(payload.entities.en.fruits.type, 'enum');
+
+    const entities = payload.entities.en.fruits.entities;
+    assert.lengthOf(entities, 2);
+    assert.equal(entities[0].name, 'orange');
+    assert.equal(entities[1].name, 'apple');
+    assert.isArray(entities[1].aliases);
+    assert.lengthOf(entities[1].aliases, 2);
+    assert.equal(entities[1].aliases[0], 'golden apple');
+    assert.equal(entities[1].aliases[1], 'granny smith apple');
   });
 
-  it('set the entity payload from message and append to previous', () => {
+  it('set the entity payload from message and append to previous', async () => {
     const msg = RED.createMessage({
       entities: {
         en: {
-          fruits: [
-            { name: 'banana' }
-          ]
+          fruits: {
+            type: 'enum',
+            entities: [
+              { name: 'banana' }
+            ]
+          }
         }
       }
     });
@@ -88,22 +124,66 @@ describe('Chat nlp entity node', () => {
     });
     NLPEntityBlock(RED);
     RED.node.get().emit('input', msg);
+    await RED.node.get().await()
+
+    const payload = RED.node.message().payload;
+    assert.isObject(payload);
+    assert.isObject(payload.entities);
+    assert.isObject(payload.entities.en);
+    assert.isObject(payload.entities.en.fruits);
+    assert.equal(payload.entities.en.fruits.type, 'enum');
+
+    const entities = payload.entities.en.fruits.entities;
+    assert.lengthOf(entities, 3);
+    assert.equal(entities[0].name, 'banana');
+    assert.equal(entities[1].name, 'orange');
+    assert.equal(entities[2].name, 'apple');
+    assert.isArray(entities[2].aliases);
+    assert.lengthOf(entities[2].aliases, 2);
+    assert.equal(entities[2].aliases[0], 'golden apple');
+    assert.equal(entities[2].aliases[1], 'granny smith apple');
+  });
+
+  it('set the entity with a valid regular expression', async () => {
+    const msg = RED.createMessage();
+    RED.node.config({
+      name: 'fruits',
+      language: 'en',
+      entityType: 'regex',
+      regex: '/[A-Z]{1,5}/'
+    });
+    NLPEntityBlock(RED);
+    RED.node.get().emit('input', msg);
+    await RED.node.get().await()
+
+    const payload = RED.node.message().payload;
+    assert.isObject(payload);
+    assert.isObject(payload.entities);
+    assert.isObject(payload.entities.en);
+    assert.isObject(payload.entities.en.fruits);
+    assert.equal(payload.entities.en.fruits.type, 'regex');
+    assert.equal(payload.entities.en.fruits.regex, '/[A-Z]{1,5}/');
+  });
+
+  it('set the entity with an invalid regular expression',  () => {
+    const msg = RED.createMessage();
+    RED.node.config({
+      name: 'fruits',
+      language: 'en',
+      entityType: 'regex',
+      regex: 'e+['
+    });
+    NLPEntityBlock(RED);
+    RED.node.get().emit('input', msg);
     return RED.node.get().await()
-      .then(() => {
-        const payload = RED.node.message().payload;
-        assert.isObject(payload);
-        assert.isObject(payload.entities);
-        assert.isObject(payload.entities.en);
-        assert.isArray(payload.entities.en.fruits);
-        assert.lengthOf(payload.entities.en.fruits, 3);
-        assert.equal(payload.entities.en.fruits[0].name, 'banana');
-        assert.equal(payload.entities.en.fruits[1].name, 'orange');
-        assert.equal(payload.entities.en.fruits[2].name, 'apple');
-        assert.isArray(payload.entities.en.fruits[2].aliases);
-        assert.lengthOf(payload.entities.en.fruits[2].aliases, 2);
-        assert.equal(payload.entities.en.fruits[2].aliases[0], 'golden apple');
-        assert.equal(payload.entities.en.fruits[2].aliases[1], 'granny smith apple');
-      });
+      .then(
+        () => {
+
+        },
+        e => {
+          assert.equal(e, 'Invalid NLP.js regex for entity "fruits"');
+        }
+      )
   });
 
 });
