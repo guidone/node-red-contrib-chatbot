e1b8af5be648fc3543033f3d1a319e202ebb852c
Guidone <guido.bellomo@sinch.com>
2026-05-11 15:13:56 +0200

    feat: enable mission control by default

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-  REDBOT_ENABLE_MISSION_CONTROL: false,
+  REDBOT_ENABLE_MISSION_CONTROL: 'true',
   REDBOT_DB_PATH: undefined,
   REDBOT_ENVIRONMENT: 'production',
   REDBOT_DEVELOPMENT_MODE: 'production',
